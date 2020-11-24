9622780968d8884da55090ae6b5c77512dba1378
ananyapillai01 <ananyaspillai@gmail.com>
2020-11-24 15:25:29 +0530

    created slingshot

---
+++ Slingshot.js
@@ -0,0 +1,27 @@
+class Slingshot{
+    constructor(bodyA, point){
+        var options = {
+            bodyA: bodyA,
+            pointB: point,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+    }
+
+    fly(){
+        this.chain.bodyA = null;
+        //null means nothing or empty 
+    }
+
+    display(){
+       if(this.chain.bodyA!=null){
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.chain.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+       }
+    }
+    
+}
